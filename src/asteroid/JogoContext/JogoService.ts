edcc380eabd0484bb3a4b1188b0d004b0da49ba4
Adriano Waltrick <adriano.waltrick@amaiseducacao.com.br>
2023-03-13 12:59:42 -0300

    feat: inteface definindo as regras de negocio e requisitos funcionais

--- src/asteroid/JogoContext/JogoService.ts
+++ src/asteroid/JogoContext/JogoService.ts
@@ -1,6 +1,8 @@
+import { JogoInterface } from "./JogoInterface";
 import { LogService } from "../SharedContext/LogService";
 
-export class JogoService {
+
+export class JogoService implements JogoInterface {
     iniciarJogo() : void {
         LogService.logarTexto('Jogo iniciado');
     }
