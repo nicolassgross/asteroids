255fcd9f24d70c36a356757ff9fd78de00c094bd
Adriano Waltrick <adriano.waltrick@amaiseducacao.com.br>
2023-04-14 12:57:16 -0300

    wip: entrada e saida da tela sincronizadas

--- src/exemplos/test-phaser.ts
+++ src/exemplos/test-phaser.ts
@@ -7,11 +7,16 @@ export default class Demo extends Phaser.Scene
     y : number;
     cursors : any;
     triangle: any;
+    triangle_copy : any;
 
     constructor ()
     {
         super('demo');
         this.x = this.y = 10;
+
+        // this.triangle = new Phaser.GameObjects.Triangle(this, 200, 200, 0, 148, 148, 148, 74, 0, 0x6666ff);
+        // this.triangle_copy = new Phaser.GameObjects.Triangle(this, 0);
+        // constructor(scene: Phaser.Scene, x?: number, y?: number, x1?: number, y1?: number, x2?: number, y2?: number, x3?: number, y3?: number, fillColor?: number, fillAlpha?: number);
         // this.graphic = this.add.graphics({ lineStyle: { color: 0x00ffff } });
 
         // this.triangle = this.add.triangle(200, 200, 0, 148, 148, 148, 74, 0, 0x6666ff);
@@ -29,6 +34,7 @@ export default class Demo extends Phaser.Scene
         this.x = this.y = 10;
 
         this.triangle = this.add.triangle(200, 200, 0, 148, 148, 148, 74, 0, 0x6666ff);
+        this.triangle_copy = this.add.triangle(200, 200, 0, 148, 148, 148, 74, 0, 0x6666ff);
 
     }
 
@@ -42,14 +48,33 @@ export default class Demo extends Phaser.Scene
         this.cursors = this.input.keyboard?.createCursorKeys();
 
         if (this.cursors.right.isDown) {
-            //  this.triangle.body.position.y += 10;
-            console.log(this.triangle.x += 10);
+            this.triangle.x += 10;
+            this.triangle_copy.x += 10;
         }
 
         if (this.cursors.left.isDown) {
-            //  this.triangle.body.position.y += 10;
-            console.log(this.triangle.x -= 10);
+            this.triangle.x -= 10;
+            this.triangle_copy.x -= 10;
         }
+
+        if (this.triangle.x >= 800) {
+            this.triangle_copy.x = this.triangle.x - 800;
+        }
+
+        if (this.triangle.x >= (800 + 200)) {
+            this.triangle.x = 200;
+        }
+
+        if (this.triangle.x <= 0) {
+            this.triangle_copy.x = this.triangle.x + 800;
+        }
+
+        if (this.triangle.x <= (0 - 200) ) {
+            this.triangle_copy.x = this.triangle.x + 800;
+            this.triangle.x = 800 - 200;
+        }
+
+
     }
 }
 
